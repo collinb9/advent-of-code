9c8c0d610901c5c8181b954912ad371d3be974f8
collinb9 <brendan.m.collins@outlook.com>
2021-12-17 14:21:50 +0100

    Tidy up 12

--- 2021/12.py
+++ 2021/12.py
@@ -1,20 +1,35 @@
 import sys
 import collections
 
+START = "start"
+END = "end"
 
 def read_data(fpath):
     with open(fpath, "r") as fp:
         data = [line.strip().split("-") for line in fp.readlines()]
-    return data
+    graph = {}
+
+    # parse data into a graph structure
+    for edge in data:
+        if START in edge:
+            graph.setdefault(START, []).append(
+                edge[(edge.index(START) + 1) % 2]
+            )
+        elif END in edge:
+            graph.setdefault(edge[(edge.index(END) + 1) % 2], []).append(END)
+        else:
+            graph.setdefault(edge[0], []).append(edge[1])
+            graph.setdefault(edge[1], []).append(edge[0])
+    return graph
 
 
-def next_nodes_problem_1(current_path, node):
+def next_nodes_problem_1(current_path, node, graph):
     for next_node in graph[node]:
         if next_node.isupper() or next_node not in current_path:
             yield next_node
 
 
-def next_nodes_problem_2(current_path, node):
+def next_nodes_problem_2(current_path, node, graph):
     counter = collections.Counter([n for n in current_path if n.islower()])
     for next_node in graph[node]:
         if (
@@ -29,42 +44,23 @@ def main(next_node_strategy):
     """
     DFS. Not very fast, but it works ...
     """
-
-    stack = collections.deque()
-    paths = []
-    stack.append(start)
+    graph = read_data(fpath)
 
     def inner(node, current_path):
         current_path.append(node)
-        if node != end:
-            for next_node in next_node_strategy(current_path[:], node):
+        if node != END:
+            for next_node in next_node_strategy(current_path[:], node, graph):
                 yield from inner(next_node, current_path[:])
         else:
             yield current_path
 
-    paths = list(inner(start, []))
+    paths = list(inner(START, []))
     return len(paths)
 
 
 if __name__ == "__main__":
     fpath = sys.argv[1]
 
-    data = read_data(fpath)
-    graph = {}
-    start = "start"
-    end = "end"
-
-    # parse data into a graph structure
-    for edge in data:
-        if start in edge:
-            graph.setdefault(start, []).append(
-                edge[(edge.index(start) + 1) % 2]
-            )
-        elif end in edge:
-            graph.setdefault(edge[(edge.index(end) + 1) % 2], []).append(end)
-        else:
-            graph.setdefault(edge[0], []).append(edge[1])
-            graph.setdefault(edge[1], []).append(edge[0])
 
     answer_1 = main(next_nodes_problem_1)
     print("Answer 1: ", answer_1)
